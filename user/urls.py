8bcc5c726201d9b81f06307e2f04105beebce831
Manish <maneeshbalami@gmail.com>
2024-02-04 14:00:36 +0545

    deleted unnecessary urls and views for signup and login

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,5 @@ from .views import HomePageView, CustomRegisterPage
 urlpatterns = [
   path('accounts/', include("allauth.urls")),
   path('accounts/register/', CustomRegisterPage.as_view(), name='register'),
-  # path('signup/', CustomUserCreationView.as_view(), name='signup'),
   path('', HomePageView.as_view(), name='home'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,5 @@
-# from django.urls import reverse_lazy
-# from django.views import generic
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-# from .forms import CustomUserCreationForm
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -11,8 +7,3 @@ class HomePageView(TemplateView):
 
 class CustomRegisterPage(TemplateView):
   template_name = 'register.html'
-
-# class SignupPageView(generic.CreateView):
-#   form_class = CustomUserCreationForm
-#   success_url = reverse_lazy('account_login')
-#   template_name = 'registration/signup.html'
