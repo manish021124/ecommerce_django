007a815aefd7f24993fa11d185ee1135cea0d316
Manish <maneeshbalami@gmail.com>
2024-03-07 17:58:00 +0545

    updated customer register page and added password visibility on and off functionality

--- user/static/js/account.js
+++ user/static/js/account.js
@@ -1,29 +1,20 @@
-// const registerPwdInput = document.getElementById("register-pwd");
-// const registerPwdConfirmInput = document.getElementById("register-confirm-pwd");
-// const loginPwdInput = document.getElementById("login-pwd");
-// const registerPwdImage = document.getElementById("register-pwd-visibility-image");
-// const registerPwdConfirmImage = document.getElementById("register-pwd-confirm-visibility-image");
-// const loginPwdImage = document.getElementById("login-pwd-visibility-image");
-// const accordionRegisterPwdInput = document.getElementById("accordion-register-pwd");
-// const accordionRegisterPwdConfirmInput = document.getElementById("accordion-register-confirm-pwd");
-// const accordionLoginPwdInput = document.getElementById("accordion-login-pwd");
-// const accordionRegisterPwdImage = document.getElementById("accordion-register-pwd-visibility-image");
-// const accordionRegisterPwdConfirmImage = document.getElementById("accordion-register-pwd-confirm-visibility-image");
-// const accordionLoginPwdImage = document.getElementById("accordion-login-pwd-visibility-image");
+const registerPwdInput = document.getElementById("password1");
+const registerPwdConfirmInput = document.getElementById("password2");
+const loginPwdInput = document.getElementById("password");
+const registerPwdImage = document.getElementById("register-pwd-visibility-image");
+const registerPwdConfirmImage = document.getElementById("register-pwd-confirm-visibility-image");
+const loginPwdImage = document.getElementById("login-pwd-visibility-image");
 
-// registerPwdImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdInput, registerPwdImage) });
-// registerPwdConfirmImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdConfirmInput, registerPwdConfirmImage) });
-// loginPwdImage.addEventListener("click", function () { togglePasswordVisibility(loginPwdInput, loginPwdImage) });
-// accordionRegisterPwdImage.addEventListener("click", function () { togglePasswordVisibility(accordionRegisterPwdInput, accordionRegisterPwdImage) });
-// accordionRegisterPwdConfirmImage.addEventListener("click", function () { togglePasswordVisibility(accordionRegisterPwdConfirmInput, accordionRegisterPwdConfirmImage) });
-// accordionLoginPwdImage.addEventListener("click", function () { togglePasswordVisibility(accordionLoginPwdInput, accordionLoginPwdImage) });
+registerPwdImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdInput, registerPwdImage) });
+registerPwdConfirmImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdConfirmInput, registerPwdConfirmImage) });
+loginPwdImage.addEventListener("click", function () { togglePasswordVisibility(loginPwdInput, loginPwdImage) });
 
-// function togglePasswordVisibility(input, image) {
-//   if (input.type === "password") {
-//     input.type = "text";
-//     image.src = "/static/images/account/visibility.svg";
-//   } else {
-//     input.type = "password";
-//     image.src = "/static/images/account/visibility_off.svg";
-//   }
-// }
+function togglePasswordVisibility(input, image) {
+  if (input.type === "password") {
+    input.type = "text";
+    image.src = "/static/images/account/visibility.svg";
+  } else {
+    input.type = "password";
+    image.src = "/static/images/account/visibility_off.svg";
+  }
+}
